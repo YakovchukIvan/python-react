795d9e8e4e55e7434fb903e64296341bff65552d
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2025-07-11 19:04:07 +0300

    create python + react project

--- backend/src/init.py
+++
@@ -1,7 +0,0 @@
-from backend.src.config import settings
-from backend.src.http_client import KFSHTTPClient
-
-kfs_client = KFSHTTPClient(
-    base_url=settings.KFS_API_URL,
-    api_key=settings.KFS_API_KEY,
-)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,72 +1,34 @@
-// import SushiCard from './components/SushiCard';
-import './index.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
-import React from 'react';
-import { MailOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Menu } from 'antd';
-
-type MenuItem = Required<MenuProps>['items'][number];
-
-const items: MenuItem[] = [
-  {
-    key: 'sub1',
-    label: 'Navigation One',
-    icon: <MailOutlined />,
-    children: [
-      {
-        key: 'g1',
-        label: 'Категорія суші1',
-        type: 'group',
-        children: [
-          { key: '1', label: 'Option 1' },
-          { key: '2', label: 'Option 2' },
-        ],
-      },
-      {
-        key: 'g2',
-        label: 'Категорія суші2',
-        type: 'group',
-        children: [
-          { key: '3', label: 'Option 3' },
-          { key: '4', label: 'Option 4' },
-        ],
-      },
-    ],
-  },
-];
-
-export type SushiItem = {
-  id: string;
+interface Detail {
   title: string;
-  price: number;
-  count: number;
-  composition: string;
-  weight: number;
-  imageUrl: string;
-  category: string;
-  favorite: boolean;
-  cart: boolean;
-};
+  channel: string;
+}
 
-const App: React.FC = () => {
-  const fetchSushiItems = () => {
-    fetch('http://127.0.0.1:8000/items')
-      .then((response) => response.json())
-      .then((data) => console.log('Success:', data))
-      .catch((error) => console.error('Error:', error));
-  };
+function App() {
+  const [details, setDetails] = useState<Detail[]>([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000')
+      .then((res) => setDetails(res.data))
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
-    <Menu
-      onClick={() => fetchSushiItems()}
-      style={{ width: 256 }}
-      defaultSelectedKeys={['1']}
-      defaultOpenKeys={['sub1']}
-      mode="inline"
-      items={items}
-    />
+    <div>
+      <header>Данні з Django</header>
+      <hr />
+      {details.map((output, id) => (
+        <div key={id}>
+          <h2>{output.title}</h2>
+          <p>{output.channel}</p>
+        </div>
+      ))}
+    </div>
   );
-};
+}
 
 export default App;
